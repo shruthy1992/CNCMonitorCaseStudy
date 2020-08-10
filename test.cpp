0622f0e58bfbf54cd257cc59bacce7559ef37b16
shruthy1992 <68539055+shruthy1992@users.noreply.github.com>
2020-08-10 18:52:06 +0530

    Add test cases

--- test.cpp
+++ test.cpp
@@ -17,28 +17,36 @@ TEST(TemperatureTest1, Temp) {
 	CNCMachineTemperatureValidator _CNCMachineTempValidator1(_cNCMachineTempProvider, alaram);
 	_cNCMachineTempProvider.Add(&_CNCMachineTempValidator1);
 	_cNCMachineTempProvider.SetTemperature(20);
+	ASSERT_EQ(AlaramType::None,alaram.GetAlert());
 	_cNCMachineTempProvider.SetTemperature(40);
+	ASSERT_EQ(AlaramType::Enviornment,alaram.GetAlert());
 }
 TEST(PartDimentionTest1, Part) {
 CNCMachinePartDimentionMonitor _CNCMachinePartDimentionMonitor;
 	CNCMachinePartDimentionValidator _CNCMachinePartDimentionValidator1(_CNCMachinePartDimentionMonitor, alaram);
 	_CNCMachinePartDimentionMonitor.Add(&_CNCMachinePartDimentionValidator1);
 	_CNCMachinePartDimentionMonitor.SetPartDimention(0.2f);
+	ASSERT_EQ(AlaramType::Machine,alaram.GetAlert());
 	_CNCMachinePartDimentionMonitor.SetPartDimention(0.01f);
+	ASSERT_EQ(AlaramType::None,alaram.GetAlert());
 }
 TEST(TimeOfOperationTest1, Time) {
 	CNCMachineOperationTimeMonitor _CNCMachineTimeOfOperationMonitor;
 	CNCMachineOperationTimeValidator _CNCMachineOperationTimeValidator1(_CNCMachineTimeOfOperationMonitor, alaram);
 	_CNCMachineTimeOfOperationMonitor.Add(&_CNCMachineOperationTimeValidator1);
 	_CNCMachineTimeOfOperationMonitor.SetTimeOfOperation(4000);
+	ASSERT_EQ(AlaramType::Enviornment,alaram.GetAlert());
 	_CNCMachineTimeOfOperationMonitor.SetTimeOfOperation(100);
+	ASSERT_EQ(AlaramType::None,alaram.GetAlert());
 }
 TEST(SelfStatusCode1, Satus) {
 	CNCMachineSelfStatusMonitor _CNCMachineSelfStatusMonitor;
 	CNCMachineSelfStatusValidator _CNCMachineSelfStatusValidator1(_CNCMachineSelfStatusMonitor, alaram);
 	_CNCMachineSelfStatusMonitor.Add(&_CNCMachineSelfStatusValidator1);
 	_CNCMachineSelfStatusMonitor.SetStatusCode(enumStatusCode::AllOk);
+	ASSERT_EQ(AlaramType::None,alaram.GetAlert());
 	_CNCMachineSelfStatusMonitor.SetStatusCode(enumStatusCode::ControlBoardNotOk);
+	ASSERT_EQ(AlaramType::Machine,alaram.GetAlert());
 }
 
 
