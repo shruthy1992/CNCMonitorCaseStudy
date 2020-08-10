e08599d8495dec9c3242a4024f5fc38991aa54f9
shruthy1992 <68539055+shruthy1992@users.noreply.github.com>
2020-08-10 16:46:11 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -19,6 +19,27 @@ TEST(TemperatureTest1, Temp) {
 	_cNCMachineTempProvider.SetTemperature(20);
 	_cNCMachineTempProvider.SetTemperature(40);
 }
+TEST(PartDimentionTest1, Part) {
+CNCMachinePartDimentionMonitor _CNCMachinePartDimentionMonitor;
+	CNCMachinePartDimentionValidator _CNCMachinePartDimentionValidator1(_CNCMachinePartDimentionMonitor, alaram);
+	_CNCMachinePartDimentionMonitor.Add(&_CNCMachinePartDimentionValidator1);
+	_CNCMachinePartDimentionMonitor.SetPartDimention(0.2f);
+	_CNCMachinePartDimentionMonitor.SetPartDimention(0.01f);
+}
+TEST(TimeOfOperationTest1, Time) {
+	CNCMachineOperationTimeMonitor _CNCMachineTimeOfOperationMonitor;
+	CNCMachineOperationTimeValidator _CNCMachineOperationTimeValidator1(_CNCMachineTimeOfOperationMonitor, alaram);
+	_CNCMachineTimeOfOperationMonitor.Add(&_CNCMachineOperationTimeValidator1);
+	_CNCMachineTimeOfOperationMonitor.SetTimeOfOperation(4000);
+	_CNCMachineTimeOfOperationMonitor.SetTimeOfOperation(100);
+}
+TEST(SelfStatusCode1, Satus) {
+	CNCMachineSelfStatusMonitor _CNCMachineSelfStatusMonitor;
+	CNCMachineSelfStatusValidator _CNCMachineSelfStatusValidator1(_CNCMachineSelfStatusMonitor, alaram);
+	_CNCMachineSelfStatusMonitor.Add(&_CNCMachineSelfStatusValidator1);
+	_CNCMachineSelfStatusMonitor.SetStatusCode(enumStatusCode::AllOk);
+	_CNCMachineSelfStatusMonitor.SetStatusCode(enumStatusCode::ControlBoardNotOk);
+}
 
 
 int main(int argc, char **argv) {
