8f9100dd5849d36d3b2456919c2c2933d24c038e
shruthy1992 <68539055+shruthy1992@users.noreply.github.com>
2020-08-12 11:30:24 +0530

    Use IAlaram instead of IAlaram

--- CNCMachinePartDimentionValidator.h
+++ CNCMachinePartDimentionValidator.h
@@ -1,19 +1,19 @@
 #pragma once
 #include "I_CNCMachineUpdator.h"
 #include "CNCMachinePartDimentionMonitor.h"
-#include "Alaram.h"
+#include "IAlaram.h"
 
 
 class CNCMachinePartDimentionValidator :
 	public I_CNCMachineUpdator
 {
 public:
-	CNCMachinePartDimentionValidator(CNCMachinePartDimentionMonitor& partDimentionMonitor,Alaram& alaram);
+	CNCMachinePartDimentionValidator(CNCMachinePartDimentionMonitor& partDimentionMonitor,IAlaram& alaram);
 	~CNCMachinePartDimentionValidator();
 	virtual void Update()override;
 private:
 	void ValidatePartDimention();
 	CNCMachinePartDimentionMonitor& _CNCMachinePartDimentionMonitor;
-	Alaram& _alaram;
+	IAlaram& _alaram;
 };
 
