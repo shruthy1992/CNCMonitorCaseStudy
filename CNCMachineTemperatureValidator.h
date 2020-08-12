eebf14e6ba58a1baba9b3d3dd6db0a1637d35dc9
shruthy1992 <68539055+shruthy1992@users.noreply.github.com>
2020-08-12 11:32:47 +0530

    Use IAlaram instead of IAlaram

--- CNCMachineTemperatureValidator.h
+++ CNCMachineTemperatureValidator.h
@@ -1,17 +1,17 @@
 #pragma once
 #include"I_CNCMachineUpdator.h"
 #include "CNCMachineTemperatureMonitor.h"
-#include "Alaram.h"
+#include "IAlaram.h"
 
 class CNCMachineTemperatureValidator :public I_CNCMachineUpdator
 {
 public:
-	CNCMachineTemperatureValidator(CNCMachineTemperatureMonitor& tempMonitor, Alaram& alaram);
+	CNCMachineTemperatureValidator(CNCMachineTemperatureMonitor& tempMonitor, IAlaram& alaram);
 	~CNCMachineTemperatureValidator();
 	virtual void Update()override;
 private:
 	void ValidateTemparature();
 	CNCMachineTemperatureMonitor& _CNCMachineTemperatureMonitor;
-	Alaram& _alaram;
+	IAlaram& _alaram;
 };
 
